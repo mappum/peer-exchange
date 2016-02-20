5ceaa9040ce75943ccc4277d22b66d09d23b730b
Matt Bell <mappum@gmail.com>
2016-02-19 21:31:38 -0800

    Refactored handshake code

--- src/peer.js
+++ src/peer.js
@@ -26,6 +26,8 @@ function Peer (id, opts) {
   this.mux = null
   this._exchangeChannel = null
   this._dataChannel = null
+  this._receivedHello = false
+  this._receivedHelloack = false
 
   this._error = this._error.bind(this)
 }
@@ -43,14 +45,12 @@ Peer.prototype.connect = function (socket) {
   this.socket = socket
   this.socket.on('error', this._error)
   this.socket.on('close', () => this.emit('close'))
-
+  this.socket.once('data', this._onHeader.bind(this))
   this.socket.write(MAGIC_HEADER)
-  read(this.socket, this._onHeader.bind(this))
 }
 
 Peer.prototype._onHeader = function (data) {
-  var magic = data.slice(0, MAGIC_HEADER.length)
-  if (magic.compare(MAGIC_HEADER) !== 0) {
+  if (data.compare(MAGIC_HEADER) !== 0) {
     return this._error(new Error('Invalid magic bytes'))
   }
 
@@ -59,53 +59,70 @@ Peer.prototype._onHeader = function (data) {
   this.mux.pipe(this.socket).pipe(this.mux)
 
   this._exchangeChannel = this.createObjectChannel('pxp')
+  this._exchangeChannel.on('data', (data) => this._exchangeChannel.emit('message:' + data.command, data))
+
   this._dataChannel = this.createChannel('data')
   this.read = this._dataChannel.read.bind(this._dataChannel)
   this.write = this._dataChannel.write.bind(this._dataChannel)
   this._dataChannel.on('data', (data) => this.emit('data', data))
 
-  this._handshake()
+  this._exchangeChannel.on('message:hello', this._onHello.bind(this))
+  this._exchangeChannel.on('message:helloack', this._onHelloack.bind(this))
+  this._sendHello()
 }
 
-Peer.prototype._handshake = function () {
+Peer.prototype._onHello = function (message) {
+  if (this._receivedHello) {
+    return this._error(new Error('Received a duplicate "hello" message'))
+  }
+  if (typeof message !== 'object') {
+    return this._error(new Error('Invalid hello message'))
+  }
+  if (message.id !== this.id) {
+    return this._error(new Error('Peer\'s network id ("' + message.id + '") ' +
+      'is different than expected ("' + this.id + '")'))
+  }
+  if (message.version !== PROTOCOL_VERSION) {
+    return this._error(new Error('Peer is using a different protocol version ' +
+      '(theirs: ' + message.version + ', ours: ' + PROTOCOL_VERSION + ')'))
+  }
+  this._remoteAccepts = message.accepts
+  this._receivedHello = true
+  this._exchangeChannel.write({ command: 'helloack' })
+  // TODO: connect to peer via other transports to verify accepts are valid
+  this._maybeReady()
+}
+
+Peer.prototype._onHelloack = function (message) {
+  if (this._receivedHelloack) {
+    return this._error(new Error('Received a duplicate "helloack" message'))
+  }
+  this._receivedHelloack = true
+  this._maybeReady()
+}
+
+Peer.prototype._maybeReady = function () {
+  if (this._receivedHello && this._receivedHelloack) {
+    this.emit('ready')
+  }
+}
+
+Peer.prototype._sendHello = function () {
   var accepts = {}
   for (var transportId in this._accepts) {
     accepts[transportId] = this._accepts[transportId].opts
   }
 
   this._exchangeChannel.write({
+    command: 'hello',
     id: this.id,
     version: PROTOCOL_VERSION,
     accepts
   })
-
-  read(this._exchangeChannel, (message) => {
-    if (typeof message !== 'object') {
-      return this._error(new Error('Invalid handshake message'))
-    }
-    if (message.id !== this.id) {
-      return this._error(new Error('Peer\'s network id ("' + message.id + '") ' +
-        'is different than expected ("' + this.id + '")'))
-    }
-    if (message.version !== PROTOCOL_VERSION) {
-      return this._error(new Error('Peer is using a different protocol version ' +
-        '(theirs: ' + message.version + ', ours: ' + PROTOCOL_VERSION + ')'))
-    }
-    this._remoteAccepts = message.accepts
-
-    this._exchangeChannel.write('ack')
-    read(this._exchangeChannel, (message) => {
-      if (message !== 'ack') {
-        return this._error(new Error('Invalid handshake ack'))
-      }
-      this.emit('ready')
-    })
-  })
 }
 
 Peer.prototype.createObjectChannel = function (id, opts) {
-  var muxStream = this.mux.createSharedStream(id, opts)
-  muxStream.on('error', this._error)
+  var muxStream = this.createChannel(id, opts)
 
   var parse = ndjson.parse()
   muxStream.pipe(parse)
@@ -128,16 +145,6 @@ Peer.prototype.getNewPeer = function (cb) {
 
 }
 
-Peer.prototype.send = function (data) {
-  this._exchangeChannel
-}
-
 Peer.prototype.destroy = function () {
   this.socket.destroy()
-}
-
-function read (stream, cb) {
-  var data = stream.read()
-  if (data) return cb(data)
-  stream.once('data', cb)
 }
